3865406e829bb92e2713e6c8903d930703b4b9f7
MTA111 <133819586+MTA111@users.noreply.github.com>
2024-05-28 18:04:14 +0300

    Update const.py

--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,9 +1,18 @@
-"""Constants for integration_blueprint."""
+"""Constants for hass_nuki_bt."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "Nuki BT"
+DOMAIN = "hass_nuki_bt"
+MANUFACTURER = "nuki"
 VERSION = "0.0.0"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+
+# config
+CONF_DEVICE_ADDRESS = "device_address"
+CONF_AUTH_ID = "auth_id"
+CONF_PRIVATE_KEY = "private_key"
+CONF_PUBLIC_KEY = "public_key"
+CONF_DEVICE_PUBLIC_KEY = "device_public_key"
+CONF_APP_ID = "app_id"
+CONF_CLIENT_TYPE = "client_type"
